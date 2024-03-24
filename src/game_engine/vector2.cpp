cfd6419c4d9340e25c83b319612966c128bb203d
bokrosf <bokros.feri@hotmail.com>
2024-03-24 22:58:28 +0100

    vector2 class added.

---
+++ src/game_engine/vector2.cpp
@@ -0,0 +1,102 @@
+#include <cmath>
+#include "vector2.h"
+
+vector2::vector2()
+    : vector2(0, 0)
+{
+}
+
+vector2::vector2(float x, float y)
+    : _x(x)
+    , _y(y)
+{
+}
+
+bool operator==(const vector2 &lhs, const vector2 &rhs)
+{
+    return lhs._x == rhs._x && lhs._y == rhs._y;
+}
+
+bool operator!=(const vector2 &lhs, const vector2 &rhs)
+{
+    return !(lhs == rhs);
+}
+
+vector2 &vector2::operator+=(const vector2 &rhs)
+{
+    _x += rhs._x;
+    _y += rhs._y;
+
+    return *this;
+}
+
+vector2 operator+(const vector2 &lhs, const vector2 rhs)
+{
+    return vector2(lhs) += rhs;
+}
+
+vector2 &vector2::operator-=(const vector2 &rhs)
+{
+    _x -= rhs._x;
+    _y -= rhs._y;
+
+    return *this;
+}
+
+vector2 operator-(const vector2 &lhs, const vector2 &rhs)
+{
+    return vector2(lhs) -= rhs;
+}
+
+vector2 &vector2::operator*=(float rhs)
+{
+    _x *= rhs;
+    _y *= rhs;
+
+    return *this;
+}
+
+vector2 operator*(float lhs, const vector2 &rhs)
+{
+    return vector2(rhs) *= lhs;
+}
+
+vector2 operator*(const vector2 &lhs, float rhs)
+{
+    return vector2(lhs) *= rhs;
+}
+
+vector2 vector2::operator-() const
+{
+    return *this * -1.0f;
+}
+
+float vector2::x() const
+{
+    return _x;
+}
+
+float vector2::y() const
+{
+    return _y;
+}
+
+float vector2::magnitude() const
+{
+    return std::sqrt(_x * _x + _y * _y);
+}
+
+vector2 vector2::normalize() const
+{
+    return *this * (1.0f / magnitude());
+}
+
+vector2 vector2::points_to(const vector2 &position) const
+{
+    return position - *this;
+}
+
+float vector2::distance_from(const vector2 &position) const
+{
+    return points_to(position).magnitude();
+}
