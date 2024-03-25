d43e8b65b61b2c68ebf781ef44d82ab1dbffb402
bokrosf <bokros.feri@hotmail.com>
2024-03-25 11:41:58 +0100

    component class added.

---
+++ src/game_engine/gameplay/component.cpp
@@ -0,0 +1,15 @@
+#include "component.h"
+
+component::component(game_object &attached_to)
+    : _attached_to(attached_to)
+{
+}
+
+component::~component()
+{
+}
+
+game_object &component::attached_to() const
+{
+    return _attached_to;
+}
