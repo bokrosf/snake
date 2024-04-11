2c3128c3f966d659cb929876d4c87210cf8c88a9
bokrosf <bokros.feri@hotmail.com>
2024-04-11 16:32:27 +0200

    SDL_Renderer * argument added to renderer::render method.

--- src/game_engine/component/renderer.h
+++ src/game_engine/component/renderer.h
@@ -1,12 +1,13 @@
 #ifndef SNAKE_GAMEENGINE_COMPONENT_RENDERER_H
 #define SNAKE_GAMEENGINE_COMPONENT_RENDERER_H
 
+#include <SDL2/SDL.h>
 #include "component.h"
 
 class renderer : public component
 {
 public:
-    virtual void render() = 0;
+    virtual void render(SDL_Renderer *renderer) = 0;
 protected:
     renderer(game_object &attached_to);
 };
