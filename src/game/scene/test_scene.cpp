b2889f8e49ca2c71ef96ff474afedb53437245df
bokrosf <bokros.feri@hotmail.com>
2024-04-15 00:48:08 +0200

    test_scene modified for testing snake rendering.

--- src/game/scene/test_scene.cpp
+++ src/game/scene/test_scene.cpp
@@ -1,6 +1,14 @@
 #include "test_scene.h"
+#include <game_engine/game_object.h>
+#include "../snake/snake_renderer.h"
+#include "../snake/tiled_movement_system.h"
 
 void test_scene::initialize()
 {
     // Write logic to test.
+    game_object *snake = new game_object();
+    snake->add_component<::snake>(vector2(500, 300), vector2(1300, 300));
+    snake->add_component<tiled_movement_system>(70);
+    snake->add_component<snake_renderer>(2, 50);
+    update_root_status(*snake);
 }
