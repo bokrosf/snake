07d4d782c799cbce7c45764ef1985bff4edb6618
bokrosf <bokros.feri@hotmail.com>
2024-04-16 19:09:48 +0200

    Tile size is same as snake thickness.

--- src/game/scene/test_scene.cpp
+++ src/game/scene/test_scene.cpp
@@ -9,7 +9,7 @@ void test_scene::initialize()
     // Write logic to test.
     game_object &snake = game_object::create();
     snake.add_component<::snake>(vector2(500, 300), vector2(1300, 300));
-    snake.add_component<tiled_movement_system>(70);
+    snake.add_component<tiled_movement_system>(50);
     snake.add_component<snake_renderer>(2, 50);
     snake.add_component<snake_controller>();
     snake.attached_component<::snake>().change_speed(50);
