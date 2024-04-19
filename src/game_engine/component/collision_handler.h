3de563dbaacbce2542c45ce191a627481fbb18c8
bokrosf <bokros.feri@hotmail.com>
2024-04-19 18:48:16 +0200

    collision_handler interface added.

---
+++ src/game_engine/component/collision_handler.h
@@ -0,0 +1,12 @@
+#ifndef SNAKE_GAMEENGINE_COMPONENT_COLLISIONHANDLER_H
+#define SNAKE_GAMEENGINE_COMPONENT_COLLISIONHANDLER_H
+
+#include "collision.h"
+
+class collision_handler
+{
+public:
+    virtual void collide(const collision &collision) = 0;
+};
+
+#endif
