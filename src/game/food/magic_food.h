d0100cf69256a36ad1ae5f81eddb15b42b49fff1
bokrosf <bokros.feri@hotmail.com>
2024-04-25 19:41:16 +0200

    magic_food class added.

---
+++ src/game/food/magic_food.h
@@ -0,0 +1,37 @@
+#ifndef SNAKE_GAME_FOOD_MAGICFOOD_H
+#define SNAKE_GAME_FOOD_MAGICFOOD_H
+
+#include <concepts>
+#include "food.h"
+#include <game/special_ability/special_ability_slot.h>
+
+template<typename Ability>
+    requires std::derived_from<Ability, component>
+class magic_food : public food
+{
+public:
+    magic_food(game_object &attached_to, const vector2 &position, float nutritional_value);
+protected:
+    void eat(snake &snake) override;
+};
+
+template<typename Ability>
+    requires std::derived_from<Ability, component>
+magic_food<Ability>::magic_food(game_object &attached_to, const vector2 &position, float nutritional_value)
+    : food(attached_to, position, nutritional_value)
+{
+}
+
+template<typename Ability>
+    requires std::derived_from<Ability, component>
+void magic_food<Ability>::eat(snake &snake)
+{
+    snake.grow(_nutritional_value);
+
+    if (special_ability_slot *ability_slot = attached_to().find_component<special_ability_slot>())
+    {
+        ability_slot->add<Ability>();
+    }
+}
+
+#endif
