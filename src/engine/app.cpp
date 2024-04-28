1e8cee1c66d593d31228e7b28b47034ce7789795
bokrosf <bokros.feri@hotmail.com>
2024-04-28 22:18:07 +0200

    Window created with the size of the current display.

--- src/engine/app.cpp
+++ src/engine/app.cpp
@@ -1,6 +1,7 @@
 #include <map>
 #include <queue>
 #include <ranges>
+#include <sstream>
 #include <vector>
 #include <engine/app.h>
 #include <engine/component/behavior.h>
@@ -84,7 +85,23 @@ void app::initialize_subsystems()
         throw subsystem_initialization_failed(std::string("SDL Video initialization failed. ").append(SDL_GetError()));
     }
 
-    SDL_Window *window = SDL_CreateWindow(_app_name.c_str(), 0, 0, 1920, 1080, SDL_WINDOW_SHOWN);
+    if (int display_count = SDL_GetNumVideoDisplays(); display_count < 1)
+    {
+        throw subsystem_initialization_failed(std::string("No video display available.").append(SDL_GetError()));
+    }
+    
+    int display_index = 0;
+    SDL_DisplayMode display_mode;
+
+    if (SDL_GetCurrentDisplayMode(display_index, &display_mode) != 0)
+    {
+        std::stringstream error("Current display mode querying failed.");
+        error << "display: " << display_index << " " << SDL_GetError();
+        
+        throw subsystem_initialization_failed(error.str());
+    }
+
+    SDL_Window *window = SDL_CreateWindow(_app_name.c_str(), 0, 0, display_mode.w, display_mode.h, SDL_WINDOW_SHOWN);
 
     if (!window)
     {
