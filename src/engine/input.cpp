91239f0faf2978bfc9bcf3634b7b433db01009aa
bokrosf <bokros.feri@hotmail.com>
2024-04-29 12:24:21 +0200

    input namespace reads the events and an event can be queried. app class uses the input namespace's functionality instead event reading.

--- src/engine/input.cpp
+++ src/engine/input.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include <utility>
+#include <vector>
 #include <engine/input.h>
 
 namespace
@@ -7,14 +7,27 @@ namespace
     std::vector<SDL_Event> events;
 }
 
-void input::update_events(std::vector<SDL_Event> &&events)
+void input::read_events()
 {
-    ::events = std::forward<std::vector<SDL_Event>>(events);
+    events.clear();
+    SDL_Event current_event;
+
+    while (SDL_PollEvent(&current_event))
+    {
+        events.push_back(current_event);
+    }
 }
 
 bool input::key_down(SDL_KeyCode key)
 {
     auto predicate = [key](const SDL_Event &event) { return event.type == SDL_KEYDOWN && event.key.keysym.sym == key; };
+
+    return std::any_of(events.begin(), events.end(), predicate);
+}
+
+bool input::occured(SDL_EventType event_type)
+{
+    auto predicate = [event_type](const SDL_Event &event) { return event.type == event_type; };
 
     return std::any_of(events.begin(), events.end(), predicate);
 }
