fb774f5ed0f2c80704c7666377f5e0861068eb35
bokrosf <bokros.feri@hotmail.com>
2024-04-29 21:55:18 +0200

    Empty classic_scene class added to the project.

---
+++ src/game/scene/classic_scene.cpp
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h>
+#include <engine/collision/box_collider.h>
+#include <engine/collision/box_collider_renderer.h>
+#include <engine/collision/collision_handler.h>
+#include <engine/display.h>
+#include <engine/entity.h>
+#include <game/food/food.h>
+#include <game/food/food_renderer.h>
+#include <game/scene/classic_scene.h>
+#include <game/snake/snake_controller.h>
+#include <game/snake/snake_renderer.h>
+#include <game/snake/tiled_movement_system.h>
+#include <game/terrain/tile_renderer.h>
+#include <game/wall/wall.h>
+#include <game/wall/wall_renderer.h>
+
+void classic_scene::initialize()
+{
+    
+}
---
+++ src/game/scene/classic_scene.h
@@ -0,0 +1,12 @@
+#ifndef SNAKE_GAME_SCENE_CLASSICSCENE_H
+#define SNAKE_GAME_SCENE_CLASSICSCENE_H
+
+#include <engine/scene.h>
+
+class classic_scene : public scene
+{
+public:
+    void initialize() override;
+};
+
+#endif
