d24ef93ed2ad9ac71eea7f3dd6d5627da35eab9d
bokrosf <bokros.feri@hotmail.com>
2024-05-01 11:48:29 +0200

    tile_maze class added.

---
+++ src/game/tile_maze.h
@@ -0,0 +1,19 @@
+#ifndef SNAKE_GAME_TILEMAZE_H
+#define SNAKE_GAME_TILEMAZE_H
+
+#include <engine/component/component.h>
+#include <engine/vector2.h>
+
+class tile_maze : public component
+{
+public:
+    tile_maze(entity &attached_to, const vector2 &center, float tile_size);
+    ~tile_maze() = default;
+    vector2 center() const;
+    vector2 tile_center(const vector2 &position) const;
+private:
+    const vector2 _center;
+    const float _tile_size;
+};
+
+#endif
