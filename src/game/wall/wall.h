dcd02bae183db6db1defe1b823d47eeebb32d7a4
bokrosf <bokros.feri@hotmail.com>
2024-05-03 17:08:18 +0200

    wall class sends game_lost message when collides with a snake. #101

--- src/game/wall/wall.h
+++ src/game/wall/wall.h
@@ -2,14 +2,17 @@
 #define SNAKE_GAME_WALL_WALL_H
 
 #include <engine/component/behavior.h>
+#include <engine/collision/collision_handler.h>
+#include <engine/entity.h>
 #include <engine/vector2.h>
 
-class wall : public behavior
+class wall : public behavior, public collision_handler
 {
 public:
     wall(entity &attached_to, const vector2 &position, const vector2 &area);
     const vector2 &position() const;
     const vector2 &area() const;
+    void collide(const collision &collision) override;
 private:
     vector2 _position;
     vector2 _area;
