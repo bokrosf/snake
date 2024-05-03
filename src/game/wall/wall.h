c742f16aeae2385fd180d06dfb746946706004ec
bokrosf <bokros.feri@hotmail.com>
2024-05-03 17:09:48 +0200

    wall explicitly implements destructor. #107

--- src/game/wall/wall.h
+++ src/game/wall/wall.h
@@ -10,6 +10,7 @@ class wall : public behavior, public collision_handler
 {
 public:
     wall(entity &attached_to, const vector2 &position, const vector2 &area);
+    ~wall() override = default;
     const vector2 &position() const;
     const vector2 &area() const;
     void collide(const collision &collision) override;
