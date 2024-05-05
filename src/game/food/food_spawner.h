6d1ed992429452e9914f29de920058e8f859e5d5
bokrosf <bokros.feri@hotmail.com>
2024-05-05 21:34:01 +0200

    Added food instantiation and initialization to food_spawner::spawn(). #102

--- src/game/food/food_spawner.h
+++ src/game/food/food_spawner.h
@@ -4,15 +4,17 @@
 #include <engine/component/component.h>
 #include <engine/entity.h>
 #include <engine/component/initializable.h>
+#include <engine/component/startable.h>
 #include <game/game_event.h>
 #include <game/tile_maze/tile_maze.h>
 
-class food_spawner : public component, public initializable
+class food_spawner : public component, public initializable, public startable
 {
 public:
     food_spawner(entity &attached_to, int food_count);
     ~food_spawner() override = default;
     void initialize() override;
+    void start() override;
     void spawn();
 private:
     int _remaining_food_count;
