0b58db68c7be608eb33eaec556dc66a0b1afa596
bokrosf <bokros.feri@hotmail.com>
2024-05-16 22:02:56 +0200

    pause_screen inherits from menu_screen. #96

--- src/game/ui/screen/pause_screen.cpp
+++ src/game/ui/screen/pause_screen.cpp
@@ -3,10 +3,7 @@
 #include <engine/input.h>
 #include <engine/rendering/image_renderer.h>
 #include <engine/rendering/material.h>
-#include <engine/vector2.h>
 #include <game/assets/asset_paths.h>
-#include <game/ui/border.h>
-#include <game/ui/border_renderer.h>
 #include <game/ui/screen/pause_screen.h>
 #include <game/scene/scene_navigator.h>
 
@@ -17,14 +14,10 @@ namespace
         resume = 0,
         exit
     };
-    
-    const int default_layer = 0;
 }
 
 pause_screen::pause_screen(entity &attached_to, const screen_configuration &configuration)
-    : behavior(attached_to)
-    , _configuration(configuration)
-    , _selected_item_index(static_cast<size_t>(menu_option::resume))
+    : menu_screen(attached_to, configuration)
 {
 }
 
@@ -35,7 +28,7 @@ void pause_screen::initialize()
     entity &title = entity::create();
     title.attach_to(&attached_to());
     title.transformation().position(vector2(display_mode.w / 2, display_mode.h / 4));
-    image_renderer &title_renderer = title.add_component<image_renderer>(default_layer);
+    image_renderer &title_renderer = title.add_component<image_renderer>(_default_rendering_layer);
     title_renderer.change_material(material{.texture_path = asset_paths::pause_title_image});
 
     add_menu_item(asset_paths::resume_image);
@@ -50,50 +43,15 @@ void pause_screen::update()
     {
         resume();
     }
-    else if (input::key_down(SDLK_UP))
+    else
     {
-        select_item(_selected_item_index + 1);
+        interact();
     }
-    else if (input::key_down(SDLK_DOWN))
-    {
-        select_item(_selected_item_index - 1);
-    }
-    else if (input::key_down(SDLK_RETURN) || input::key_down(SDLK_SPACE))
-    {
-        confirm();
-    }
-}
-
-void pause_screen::add_menu_item(const std::string &image_path)
-{
-    entity &item = entity::create();
-    item.attach_to(&attached_to());
-    
-    border &border = item.add_component<::border>();
-    border.transformation().position(_configuration.first_item_position + vector2(0, _menu_items.size() * _configuration.item_spacing));
-    border.inner_area(_configuration.item_area);
-    border.thickness(_configuration.border_thickness);
-    border_renderer &border_renderer = item.add_component<::border_renderer>(default_layer);
-    border_renderer.activate(false);
-    border_renderer.change_material({.color = _configuration.item_border_color});
-
-    image_renderer &image_renderer = item.add_component<::image_renderer>(default_layer);
-    image_renderer.change_material(material{.texture_path = image_path});
-
-    _menu_items.push_back(&item);
-}
-
-void pause_screen::select_item(size_t index)
-{
-    _menu_items[_selected_item_index]->attached_component<border_renderer>().activate(false);
-    index %= _menu_items.size();
-    _menu_items[index]->attached_component<border_renderer>().activate(true);
-    _selected_item_index = index;
 }
 
 void pause_screen::confirm()
 {
-    switch (static_cast<menu_option>(_selected_item_index))
+    switch (static_cast<menu_option>(selected_item_index()))
     {
         case menu_option::resume:
             resume();
--- src/game/ui/screen/pause_screen.h
+++ src/game/ui/screen/pause_screen.h
@@ -1,29 +1,18 @@
 #ifndef SNAKE_GAME_UI_SCREEN_PAUSESCREEN_H
 #define SNAKE_GAME_UI_SCREEN_PAUSESCREEN_H
 
-#include <string>
-#include <vector>
-#include <engine/component/behavior.h>
-#include <engine/component/initializable.h>
-#include <engine/component/updatable.h>
-#include <engine/entity.h>
-#include <game/ui/screen/screen_configuration.h>
+#include <game/ui/screen/menu_screen.h>
 
-class pause_screen : public behavior, public initializable, public updatable
+class pause_screen : public menu_screen
 {
 public:
     pause_screen(entity &attached_to, const screen_configuration &configuration);
     void initialize() override;
     void update() override;
+protected:
+    void confirm() override;
 private:
-    void add_menu_item(const std::string &image_path);
-    void select_item(size_t index);
-    void confirm();
     void resume();
-
-    const screen_configuration _configuration;
-    std::vector<entity *> _menu_items;
-    size_t _selected_item_index;
 };
 
 #endif
