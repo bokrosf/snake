4d8b966aaac4c65df369db979398a02215a228eb
bokrosf <bokros.feri@hotmail.com>
2024-05-16 22:19:35 +0200

    main_scene class added.

---
+++ src/game/scene/main_scene.cpp
@@ -0,0 +1,8 @@
+#include <engine/display.h>
+#include <game/scene/main_scene.h>
+#include <game/ui/screen/screen_configuration.h>
+
+void main_scene::initialize()
+{
+    // TODO 2024-05-16 Create the main_screen.
+}
---
+++ src/game/scene/main_scene.h
@@ -0,0 +1,12 @@
+#ifndef SNAKE_GAME_SCENE_MAINSCENE_H
+#define SNAKE_GAME_SCENE_MAINSCENE_H
+
+#include <engine/scene.h>
+
+class main_scene : public scene
+{
+public:
+    void initialize() override;
+};
+
+#endif
