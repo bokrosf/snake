5962d77b826921b18655b1f19586f07ab5bca5cc
bokrosf <bokros.feri@hotmail.com>
2024-05-16 23:21:15 +0200

    magic_food_scene class added.

---
+++ src/game/scene/magic_food_scene.cpp
@@ -0,0 +1,6 @@
+#include <game/scene/magic_food_scene.h>
+
+void magic_food_scene::initialize()
+{
+    // TODO 2024-05-16 Create the scene graph.
+}
---
+++ src/game/scene/magic_food_scene.h
@@ -0,0 +1,12 @@
+#ifndef SNAKE_GAME_SCENE_MAGICFOODSCENE_H
+#define SNAKE_GAME_SCENE_MAGICFOODSCENE_H
+
+#include <engine/scene.h>
+
+class magic_food_scene : public scene
+{
+public:
+    void initialize() override;
+};
+
+#endif
