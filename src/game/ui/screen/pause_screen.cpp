3a41fe7324803ab6316fdfbdc5523af5d90a5a67
bokrosf <bokros.feri@hotmail.com>
2024-05-16 17:20:22 +0200

    Pause screen displays the title and the menu options. #96

--- src/game/ui/screen/pause_screen.cpp
+++ src/game/ui/screen/pause_screen.cpp
@@ -1,10 +1,40 @@
 #include <engine/input.h>
+#include <engine/rendering/image_renderer.h>
+#include <engine/rendering/material.h>
+#include <engine/vector2.h>
+#include <game/assets/asset_paths.h>
+#include <game/ui/border.h>
+#include <game/ui/border_renderer.h>
 #include <game/ui/screen/pause_screen.h>
 #include <game/scene/scene_navigator.h>
 
+namespace
+{
+    const int default_layer = 0;
+    const int border_thickness = 10;
+}
+
 pause_screen::pause_screen(entity &attached_to)
     : behavior(attached_to)
+    , _selected_item_index(static_cast<size_t>(menu_option::resume))
 {
+}
+
+void pause_screen::initialize()
+{
+    entity &title = entity::create();
+    title.attach_to(&attached_to());
+    title.transformation().position(vector2(1920 / 2, 1080 / 4));
+    image_renderer &title_renderer = title.add_component<image_renderer>(default_layer);
+    title_renderer.change_material(material{.texture_path = asset_paths::pause_title_image});
+
+    add_menu_item(menu_option::resume, asset_paths::resume_image);
+    add_menu_item(menu_option::exit, asset_paths::exit_image);
+
+    for (size_t i = 0; i < _menu_items.size(); ++i)
+    {
+        _menu_items[i]->transformation().translate(vector2(0, i * (128 + 4 * border_thickness)));
+    }
 }
 
 void pause_screen::update()
@@ -13,4 +43,23 @@ void pause_screen::update()
     {
         scene_navigator::instance().pop();
     }
+}
+
+void pause_screen::add_menu_item(menu_option option, const std::string &image_path)
+{
+    entity &item = entity::create();
+    item.attach_to(&attached_to());
+    
+    border &border = item.add_component<::border>();
+    border.transformation().position(0.5F * vector2(1920, 1080));
+    border.inner_area(0.5F * vector2(256, 128));
+    border.thickness(border_thickness);
+    border_renderer &border_renderer = item.add_component<::border_renderer>(default_layer);
+    border_renderer.activate(false);
+    border_renderer.change_material({.color = SDL_Color(14, 209, 69, 255)});
+
+    image_renderer &image_renderer = item.add_component<::image_renderer>(default_layer);
+    image_renderer.change_material(material{.texture_path = image_path});
+
+    _menu_items[static_cast<size_t>(option)] = &item;
 }
--- src/game/ui/screen/pause_screen.h
+++ src/game/ui/screen/pause_screen.h
@@ -1,14 +1,30 @@
 #ifndef SNAKE_GAME_UI_SCREEN_PAUSESCREEN_H
 #define SNAKE_GAME_UI_SCREEN_PAUSESCREEN_H
 
+#include <array>
+#include <string>
 #include <engine/component/behavior.h>
+#include <engine/component/initializable.h>
 #include <engine/component/updatable.h>
+#include <engine/entity.h>
 
-class pause_screen : public behavior, public updatable
+class pause_screen : public behavior, public initializable, public updatable
 {
 public:
     pause_screen(entity &attached_to);
+    void initialize() override;
     void update() override;
+private:
+    enum class menu_option
+    {
+        resume = 0,
+        exit
+    };
+
+    void add_menu_item(menu_option option, const std::string &image_path);
+
+    std::array<entity *, 2> _menu_items;
+    size_t _selected_item_index;
 };
 
 #endif
