dbf95d93444f2290d5852305a94752fefa38f0df
bokrosf <bokros.feri@hotmail.com>
2024-09-17 18:57:57 +0200

    Snake self collision detection implemented. #122

--- src/game/snake/snake.h
+++ src/game/snake/snake.h
@@ -14,7 +14,7 @@
 class snake : public behavior, public initializable, public startable, public updatable
 {
 public:
-    snake(entity &attached_to, const vector2 &head, const vector2 &tail);
+    snake(entity &attached_to, const vector2 &head, const vector2 &tail, float thickness);
     void initialize() override;
     void start() override;
     void update() override;
@@ -25,12 +25,14 @@ public:
 private:
     void move_forward();
     void shrink_tail(float removed_length);
+    void check_self_collision() const;
 
     vector2 _head_direction;
     std::list<vector2> _segments;
     movement_system *_movement_system;
     box_collider *_collider;
     float _speed;
+    const float _thickness;
 };
 
 #endif
