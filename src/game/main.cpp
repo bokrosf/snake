27fcce49f780ffd2d75da94646448efce7fcd3a9
bokrosf <bokros.feri@hotmail.com>
2024-10-04 14:18:15 +0200

    snake_app instantiated on the stack, no heap allocation required, ensured to call the destructor when the app finished.

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,6 +1,5 @@
 #include <exception>
 #include <iostream>
-#include <engine/app.h>
 #include <engine/app_configuration.h>
 #include <engine/logging/logger.h>
 #include <game/snake_app.h>
@@ -11,8 +10,8 @@ int main(int argc, char *argv[])
     
     try
     {
-        app *app = new snake_app(app_configuration{"Snake SDL"});
-        app->run();
+        snake_app app = snake_app(app_configuration{"Snake SDL"});
+        app.run();
 
         return 0;
     }
