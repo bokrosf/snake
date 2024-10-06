768e0c104c2e1fd5c43357986d8038795d2197fa
bokrosf <bokros.feri@hotmail.com>
2024-10-06 23:16:20 +0200

    tile_position struct added. #162

---
+++ src/game/tile_maze/tile_position.cpp
@@ -0,0 +1,7 @@
+#include <game/tile_maze/tile_position.h>
+
+tile_position::tile_position()
+    : row(0)
+    , column(0)
+{
+}
---
+++ src/game/tile_maze/tile_position.h
@@ -0,0 +1,12 @@
+#ifndef SNAKE_GAME_TILEMAZE_TILEPOSITION_H
+#define SNAKE_GAME_TILEMAZE_TILEPOSITION_H
+
+struct tile_position
+{
+    tile_position();
+
+    unsigned int row;
+    unsigned int column;
+};
+
+#endif
