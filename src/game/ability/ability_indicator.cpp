b7f3fb06856c1ec1240cdb4f376ff148aaa62db0
bokrosf <bokros.feri@hotmail.com>
2024-10-14 13:30:32 +0200

    ability_indicator class added. #169

---
+++ src/game/ability/ability_indicator.cpp
@@ -0,0 +1,57 @@
+#include <game/ability/ability_indicator.h>
+
+ability_indicator::ability_indicator(entity &attached_to)
+    : behavior(attached_to)
+    , _percentage(0)
+    , _ability(nullptr)
+{
+}
+
+ability_indicator::~ability_indicator()
+{
+    stop_tracking();
+}
+
+void ability_indicator::track(::ability &ability)
+{
+    stop_tracking();
+    _ability = &ability;
+    _percentage = 0.0F;
+    activate(true);
+    _messenger.subscribe<ability_usage_changed>(*this);
+    _messenger.subscribe<ability_expired>(*this);
+}
+
+float ability_indicator::percentage() const
+{
+    return _percentage;
+}
+
+void ability_indicator::receive(const ability_usage_changed &message)
+{
+    if (&message.ability == _ability)
+    {
+        _percentage = message.remaining_percentage;
+    }
+}
+
+void ability_indicator::receive(const ability_expired &message)
+{
+    if (&message.ability == _ability)
+    {
+        stop_tracking();
+    }
+}
+
+void ability_indicator::stop_tracking()
+{
+    if (!_ability)
+    {
+        return;
+    }
+
+    _messenger.unsubscribe<ability_usage_changed>(*this);
+    _messenger.unsubscribe<ability_expired>(*this);
+    _ability = nullptr;
+    activate(false);
+}
---
+++ src/game/ability/ability_indicator.h
@@ -0,0 +1,26 @@
+#ifndef SNAKE_GAME_ABILITY_ABILITYINDICATOR_H
+#define SNAKE_GAME_ABILITY_ABILITYINDICATOR_H
+
+#include <engine/component/behavior.h>
+#include <engine/messaging/recipient.h>
+#include <game/ability/ability.h>
+#include <game/ability/ability_expired.h>
+#include <game/ability/ability_usage_changed.h>
+
+class ability_indicator : public behavior, public recipient<ability_usage_changed>, public recipient<ability_expired>
+{
+public:
+    ability_indicator(entity &attached_to);
+    ~ability_indicator() override;
+    void track(::ability &ability);
+    float percentage() const;
+    void receive(const ability_usage_changed &message);
+    void receive(const ability_expired &message);
+private:
+    void stop_tracking();
+
+    float _percentage;
+    ability *_ability;
+};
+
+#endif
