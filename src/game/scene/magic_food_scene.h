5b873f009dc2487234e90f08fc7a27c33a09c3bb
bokrosf <bokros.feri@hotmail.com>
2024-10-15 13:51:25 +0200

    magic_food_scene creates a scene similar to classic_scene. #168

--- src/game/scene/magic_food_scene.h
+++ src/game/scene/magic_food_scene.h
@@ -2,12 +2,15 @@
 #define SNAKE_GAME_SCENE_MAGICFOODSCENE_H
 
 #include <engine/scene.h>
+#include <game/food/magic_food_factory.h>
 
 class magic_food_scene : public scene
 {
 public:
     magic_food_scene(int id);
     void initialize() override;
+private:
+    magic_food_factory _food_factory;
 };
 
 #endif
