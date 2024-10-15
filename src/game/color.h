db9562100a525e65e57c56db7191830eae97f573
bokrosf <bokros.feri@hotmail.com>
2024-10-15 14:50:16 +0200

    sassy_grape color added. #171

--- src/game/color.h
+++ src/game/color.h
@@ -11,6 +11,7 @@ namespace color
     const SDL_Color menu_item_border = SDL_Color{14, 209, 69, 255};
 
     const SDL_Color brave_strawberry = SDL_Color{152, 52, 45, 255};
+    const SDL_Color sassy_grape = SDL_Color{142, 45, 89, 255};
 }
 
 #endif
