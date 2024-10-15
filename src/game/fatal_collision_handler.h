635c2ebcf9fcb68faaaf36142ecad5b788ed1f79
bokrosf <bokros.feri@hotmail.com>
2024-10-15 18:20:52 +0200

    fatal_collision_handler class added. #172

---
+++ src/game/fatal_collision_handler.h
@@ -0,0 +1,15 @@
+#ifndef SNAKE_GAME_FATALCOLLISIONHANDLER_H
+#define SNAKE_GAME_FATALCOLLISIONHANDLER_H
+
+#include <engine/component/behavior.h>
+#include <engine/collision/collision_handler.h>
+#include <engine/entity.h>
+
+class fatal_collision_handler : public behavior, public collision_handler
+{
+public:
+    fatal_collision_handler(entity &attached_to);
+    void collide(const collision &collision) override;
+};
+
+#endif
