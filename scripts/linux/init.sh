937b4aec0a83532d27efdf5fb5b5443b7ad6c7b9
bokrosf <bokros.feri@hotmail.com>
2024-11-07 14:11:03 +0100

    Linux init script uses cmake for project creation. #166

--- scripts/linux/init.sh
+++ scripts/linux/init.sh
@@ -1,42 +1,14 @@
 #!/bin/sh
 
 echo "Initializing project..."
-original_path=$(pwd)
 script_path="$(dirname "$(realpath "$0")")"
-cd $script_path;
+cd "$script_path"
 cd ../..
 project_path=$(pwd)
-echo "Creating directories..."
-mkdir build
-mkdir -p lib/SDL
-mkdir dependencies
-echo "Finished creating directories."
-
-echo "Obtaining dependencies..."
-cd dependencies
-
-$script_path/obtain_dependency.sh \
-  "SDL" \
-  "release-2.30.2" \
-  "https://github.com/libsdl-org/SDL" \
-  "$project_path/lib/SDL/" \
-
-$script_path/obtain_dependency.sh \
-  "SDL_image" \
-  "release-2.8.2" \
-  "https://github.com/libsdl-org/SDL_image" \
-  "$project_path/lib/SDL/" \
-  "SDL"
-
-cd $project_path
-echo "Finished obtaining dependencies."
 
 echo "Creating build system..."
-cd build
-cmake ..
-cd $project_path
+mkdir -p build/debug
+cd build/debug
+cmake -DCMAKE_BUILD_TYPE=Debug "$project_path"
 echo "Finished creating build system."
-
-rm -rf $project_path/dependencies
-cd $original_path
 echo "Finished project initialization."
